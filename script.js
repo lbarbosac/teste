f7b22bab1d7aeb2a8b1ab854239858aebe377b69
Lucas Barbosa <lucas2304barbosa@gmail.com>
2024-08-16 11:10:31 -0300

    GRID-JS
    Fiz as marcações e add algumas coisas no js

---
+++ script.js
@@ -0,0 +1,16 @@
+// perguntando ao usuario
+let nome = prompt("Digite seu nome");
+alert("Olá "+nome+", tudo bem ?");
+
+// alerta na tela quando entra
+alert("Cuidado !");
+
+// console do navegador
+console.log("Teste de console");
+
+// inclui o HTML em um elemento indentificado pelo ID
+document.getElementById("demo").innerHTML = "Lucas Barbosa"
+
+// sorteio de numeros
+let numeroAleatorio = Math.floor(Math.random(1,100) * 100);
+console.log(numeroAleatorio);
